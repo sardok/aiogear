f21bad0c1ec877e5044028b97d1ee78c9a095191
Sinan Nalkaya <sardok@gmail.com>
2017-03-02 01:17:51 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from distutils.core import setup
 
-setup(name='aiogear',
-      version='0.1',
-      description='Async gearman protocol',
-      packages=['aiogear']
+setup(
+    name='aiogear',
+    version='0.1',
+    description='Asynchronous gearman protocol based on asyncio',
+    packages=['aiogear'],
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
 )
