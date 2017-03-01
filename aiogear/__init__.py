e7f3dcc645fc57e2ba428a17c3ef9af1eb59cc00
Sinan Nalkaya <sardok@gmail.com>
2017-03-02 00:31:00 +0300

    Export PacketType so caller could be free to send any type of packet.

--- aiogear/__init__.py
+++ aiogear/__init__.py
@@ -1,5 +1,6 @@
 from aiogear.worker import Worker
 from aiogear.client import Client
+from aiogear.packet import Type as PacketType
 
 
-__all__ = ['Worker', 'Client']
+__all__ = ['Worker', 'Client', 'PacketType']
