9428cfc87360dc397590f07835a0743afb64fa53
Sinan Nalkaya <sardok@gmail.com>
2017-04-10 20:23:27 +0300

    setup.py: bumped version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='aiogear',
-    version='0.1.4',
+    version='0.2',
     author='Sinan Nalkaya',
     author_email='sardok@gmail.com',
     url='https://github.com/sardok/aiogear',
