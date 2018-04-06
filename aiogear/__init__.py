f6cd857f09dc1fd33692123e03a0e0be10286c09
Kyle Agronick <kyle.agronick@cvscaremark.com>
2018-04-06 12:02:26 -0400

    Fixed for new version of aiogear

--- aiogear/__init__.py
+++ aiogear/__init__.py
@@ -2,6 +2,7 @@ from aiogear.worker import Worker
 from aiogear.client import Client
 from aiogear.admin import Admin
 from aiogear.packet import Type as PacketType
+from aiogear.callback_client import CallbackClient
 
 
-__all__ = ['Worker', 'Client', 'Admin', 'PacketType']
+__all__ = ['Worker', 'Client', 'Admin', 'PacketType',  'CallbackClient']
